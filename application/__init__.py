ec288c3b7f4394e3e3dde45686534fee1a2b596a
ms-starryvoid <donamattel@gmail.com>
2024-02-23 14:43:47 +0530

    db connect

--- application/__init__.py
+++ application/__init__.py
@@ -4,11 +4,15 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
 from flask_pymongo import PyMongo
-
+    #env_path='.env'
+load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
-app.config["MONGO_URI"] = "mongodb+srv://donajohn31:cluster1pswd@cluster1.62yuwb5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+m=os.environ.get('MONGO_URI') 
+#print(m)
+app.config["MONGO_URI"] = m
 # app.config["MONGO_URI"] = os.environ.get('MONGO_URI') ---NOT WOKRING 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg','tif'}
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'sample')
