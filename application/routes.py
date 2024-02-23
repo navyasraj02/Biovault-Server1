8cfc2d7b6c9a51f3af4e98d5cadeb7d09e6ae202
navyasraj02 <navyasraj2003@gmail.com>
2024-02-23 15:42:20 +0530

    nn

--- application/routes.py
+++ application/routes.py
@@ -5,16 +5,20 @@ from .route_func import fpMatch
 
 @app.route("/")
 def home():
-    
     return  {"status": "success", "message": "Connected to server 1"}
 @app.route('/api/log',methods=["GET"])
 def log():
-    id=request.get("id")
+    id=request.get("t_id")
     kp=request.get("kp")
     desc=request.get("desc")
-    eUser=db.fing1.find_one({"fid":id})
-    if eUser:
-        #match the segment retrieved with the incoming segment using match algo
-        return {"success":"true"}
-    else:
-        return {"success": "false"}
+    print("data received from main server")
+    print("id:",id)
+    print("kp:",kp)
+    print("desc:",desc)
+    return {"success":"true"}
+    # eUser=db.fing1.find_one({"fid":id})
+    # if eUser:
+    #     #match the segment retrieved with the incoming segment using match algo
+    #     return {"success":"true"}
+    # else:
+    #     return {"success": "false"}
