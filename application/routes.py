c288a083afa7cb0b5dea20baab2e9d59e3fe130a
ms-starryvoid <donamattel@gmail.com>
2024-03-29 22:43:49 +0530

    revcms further details

--- application/routes.py
+++ application/routes.py
@@ -12,20 +12,30 @@ def log():
     data=request.json
     print(data)
     #data = msgpack.loads(data)
-    """data_array1 = np.array(data["data_array1"])
-    data_array2 = data["data_array2"]
-    data_string = data["data_string"]
-    print(data_array1,data_array2,data_string)"""
-        #kp=request.get("kp")
-    #desc=request.get("desc")
+    length = data['data']['len']
+    description = data['data']['descrip']
+    user_id = data['data']['user_id']
+    retrieveddesc=0 #retieve descriptiopn from 
+    #code to retieve data from mongodb and match
+    score=fpMatch.fingerprint_segment(description,retrieveddesc)
+    
     """print("data received from main server")
-    print("id:",id)
-    print("kp:",kp)
-    print("desc:",desc)"""
-    return {"success":"true"}
+   """
+    return {"success":"true", "score":score}
     # eUser=db.fing1.find_one({"fid":id})
     # if eUser:
     #     #match the segment retrieved with the incoming segment using match algo
     #     return {"success":"true"}
     # else:
     #     return {"success": "false"}
+@app.route('/api/reg')
+def reg():
+    #code to recieve data from the server insert to mongo db with user id
+    data=request.json
+    print(data)
+    #data = msgpack.loads(data)
+    length = data['data']['len']
+    description = data['data']['descrip']
+    user_id = data['data']['user_id']
+    return
+
